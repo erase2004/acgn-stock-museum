d2c3c5d6bad8e8c5227473b5d06c1b1e40a42d60
Tsuki Akiba <erase2004@gmail.com>
2025-10-12 21:40:44 +0800

    refactor: add schema to dbRound and dbSeason

--- src/services/dbRound.ts
+++ src/services/dbRound.ts
@@ -1,4 +1,12 @@
+import { z } from 'astro/zod'
 import type { Db } from 'mongodb'
+
+export const schema = z.object({
+  /** 起始日期 */
+  beginDate: z.coerce.date(),
+  /** 結束日期 */
+  endDate: z.coerce.date(),
+})
 
 export function getDBRound(db: Db) {
   const dbRound = db.collection('round')
@@ -8,5 +16,8 @@ export function getDBRound(db: Db) {
 
 export async function getCurrentRound(db: Db) {
   const dbRound = getDBRound(db)
-  return await dbRound.findOne({}, { sort: { beginDate: -1 } })
+  const { data } = await z
+    .promise(schema)
+    .safeParse(dbRound.findOne({}, { sort: { beginDate: -1 } }))
+  return data
 }
--- src/services/dbSeason.ts
+++ src/services/dbSeason.ts
@@ -1,4 +1,20 @@
+import { z } from 'astro/zod'
 import type { Db } from 'mongodb'
+
+export const schema = z.object({
+  /** 賽季的第幾季度 */
+  ordinal: z.number().int(),
+  /** 起始日期 */
+  beginDate: z.coerce.date(),
+  /** 結束日期 */
+  endDate: z.coerce.date(),
+  /** 當季有多少驗證通過的使用者 */
+  userCount: z.number().int(),
+  /** 當季起始時有多少未被查封的公司 */
+  companiesCount: z.number().int(),
+  /** 當季有多少推出的新產品 */
+  productCount: z.number().int(),
+})
 
 export function getDBSeason(db: Db) {
   const dbSeason = db.collection('season')
@@ -8,10 +24,16 @@ export function getDBSeason(db: Db) {
 
 export async function getCurrentSeason(db: Db) {
   const dbSeason = getDBSeason(db)
-  return await dbSeason.findOne({}, { sort: { beginDate: -1 } })
+  const { data } = await z
+    .promise(schema)
+    .safeParse(dbSeason.findOne({}, { sort: { beginDate: -1 } }))
+  return data
 }
 
 export async function getPreviousSeason(db: Db) {
   const dbSeason = getDBSeason(db)
-  return await dbSeason.findOne({}, { sort: { beginDate: -1 }, skip: 1 })
+  const { data } = await z
+    .promise(schema)
+    .safeParse(dbSeason.findOne({}, { sort: { beginDate: -1 }, skip: 1 }))
+  return data
 }
