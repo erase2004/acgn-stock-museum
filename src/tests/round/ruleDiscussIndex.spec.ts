fce6ef6bc99803bb8565d48dd4418450a741caad
Tsuki Akiba <erase2004@gmail.com>
2025-11-02 19:31:43 +0800

    test(ruleDiscussionList): rewrite e2e test

--- src/tests/round/ruleDiscussIndex.spec.ts
+++ src/tests/round/ruleDiscussIndex.spec.ts
@@ -2,29 +2,26 @@ import { test, expect } from '@playwright/test'
 import { rounds, siteList } from '@/configs/sites'
 import { getRuleAgendaListUrl } from '@/libs/routes'
 
-test('has title', async ({ context }) => {
-  const tasks = rounds.map(async (round) => {
-    const websiteName = siteList[round as keyof typeof siteList]?.name
-    const title = `規則討論 - ${websiteName}`
+for (const round of rounds) {
+  test.describe(`[${round}] rule discussion list`, () => {
+    test('suites', async ({ context }) => {
+      const page = await context.newPage()
 
-    const page = await context.newPage()
-    await page.goto(getRuleAgendaListUrl(round))
-    await expect(page).toHaveTitle(title)
-  })
+      await page.goto(getRuleAgendaListUrl(round))
 
-  await Promise.all(tasks)
-})
+      await test.step('has title', async () => {
+        const websiteName = siteList[round as keyof typeof siteList]?.name
+        const title = `規則討論 - ${websiteName}`
 
-test('has content', async ({ context }) => {
-  const tasks = rounds.map(async (round) => {
-    const page = await context.newPage()
-    await page.goto(getRuleAgendaListUrl(round))
+        await expect(page).toHaveTitle(title)
+      })
 
-    const element = await page.locator('h1', {
-      hasText: '規則討論',
+      await test.step('has content', async () => {
+        const element = await page.locator('.round-block-title')
+
+        await expect(element).toHaveText('規則討論')
+        await expect(element).toBeVisible()
+      })
     })
-    await expect(element).toBeVisible()
   })
-
-  await Promise.all(tasks)
-})
+}
