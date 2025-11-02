0992850018ccf74d4b40e51c260e5475f804b994
Tsuki Akiba <erase2004@gmail.com>
2025-11-02 19:06:22 +0800

    test(round/homepage): rewrite e2e test

--- src/tests/round/index.spec.ts
+++ src/tests/round/index.spec.ts
@@ -5,33 +5,30 @@ import { getConnection } from '@/tests/_utils/database'
 import { formatDateTimeText } from '@/libs/timeFormat'
 import { getCurrentRound } from '@/services/dbRound'
 
-test('has title', async ({ context }) => {
-  const tasks = rounds.map(async (round) => {
-    const title = siteList[round as keyof typeof siteList]?.name
+for (const round of rounds) {
+  test.describe(`[${round}] homepage`, () => {
+    test('suites', async ({ context }) => {
+      const page = await context.newPage()
 
-    const page = await context.newPage()
-    await page.goto(getRoundMainPageUrl(round))
-    await expect(page).toHaveTitle(title)
+      await page.goto(getRoundMainPageUrl(round))
+
+      await test.step('has title', async () => {
+        const title = siteList[round as keyof typeof siteList]?.name
+
+        await expect(page).toHaveTitle(title)
+      })
+
+      await test.step('has round info', async () => {
+        const connection = getConnection(round)
+        const roundData = await getCurrentRound(connection)
+
+        const element = await page.getByText('當前賽季起訖時間')
+        await expect(element).toHaveCount(1)
+
+        const sibling = element.locator('//following-sibling::*')
+        await expect(sibling).toContainText(formatDateTimeText(roundData?.beginDate))
+        await expect(sibling).toContainText(formatDateTimeText(roundData?.endDate))
+      })
+    })
   })
-
-  await Promise.all(tasks)
-})
-
-test('has round info', async ({ context }) => {
-  const tasks = rounds.map(async (round) => {
-    const connection = getConnection(round)
-    const roundData = await getCurrentRound(connection)
-
-    const page = await context.newPage()
-    await page.goto(getRoundMainPageUrl(round))
-
-    const element = await page.getByText('當前賽季起訖時間')
-    await expect(element).toHaveCount(1)
-
-    const sibling = element.locator('//following-sibling::*')
-    await expect(sibling).toContainText(formatDateTimeText(roundData?.beginDate))
-    await expect(sibling).toContainText(formatDateTimeText(roundData?.endDate))
-  })
-
-  await Promise.all(tasks)
-})
+}
