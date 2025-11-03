9fbc075175bd15ee260550986e7dc63a7602546b
Tsuki Akiba <erase2004@gmail.com>
2025-11-03 21:02:42 +0800

    fix: view under mobile viewport

--- src/components/round/announcement/preact/ListContainer.tsx
+++ src/components/round/announcement/preact/ListContainer.tsx
@@ -17,27 +17,27 @@ export default function ListContainer({ round }: Props) {
       <table class="table-base custom-responsive-table-md table-pin-rows table">
         <thead>
           <tr>
-            <th class="truncate text-center" title="分類">
+            <th class="text-center" title="分類">
               分類
             </th>
-            <th class="w-1/2 truncate text-center" title="主旨">
+            <th class="w-2/5 text-center" title="主旨">
               主旨
             </th>
-            <th class="truncate text-center" title="發佈人">
+            <th class="text-center" title="發佈人">
               發佈人
             </th>
-            <th class="truncate text-center" title="發佈日期">
+            <th class="w-40 text-center" title="發佈日期">
               發佈日期
             </th>
           </tr>
         </thead>
         <tbody>
           {$items.map(({ _id, category, createdAt, voided, subject, creator }) => (
-            <tr key={_id}>
-              <td class="truncate text-center" data-title="分類">
+            <tr key={_id} class="*:px-2">
+              <td class="text-center" data-title="分類">
                 {categoryDisplayName(category)}
               </td>
-              <td class="truncate text-left" data-title="主旨">
+              <td class="text-left" data-title="主旨">
                 <div class="flex items-center gap-x-2">
                   {voided && <span class="badge text-nowrap badge-warning">已作廢</span>}
                   <a href={getAnnouncementUrl(round, _id)}>{subject}</a>
