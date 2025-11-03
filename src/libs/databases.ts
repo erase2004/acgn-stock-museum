f656531467ba072fdd5cbc6e4ccbf0012b227cfe
Tsuki Akiba <erase2004@gmail.com>
2025-11-03 09:19:57 +0800

    fix: create too many db connections during e2e tests

--- src/libs/databases.ts
+++ src/libs/databases.ts
@@ -2,11 +2,13 @@ import { MongoClient } from 'mongodb'
 import { getRoundData } from './routes'
 import { DB_URI } from 'astro:env/server'
 
+const client = new MongoClient(DB_URI)
+
 export function getConnection(round: string) {
   const roundData = getRoundData(round)
 
   if (!roundData) throw new Error('Round is not valid')
   if (roundData.disabled) throw new Error('Round lacks of data')
 
-  return new MongoClient(DB_URI).db(roundData.dbname)
+  return client.db(roundData.dbname)
 }
